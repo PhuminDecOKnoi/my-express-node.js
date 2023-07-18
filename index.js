4b9e3adb626da2ac41a62e105c18b63cbcce4471
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-18 22:58:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,16 +4,9 @@ const path = require('path'); //โหลดโมดูล path
 app.use(function(request, response){
     response.status(200);
     response.type('text/html');
-    response.sendFile(path.join(__dirname,'index.html'));
-    // response.send('<!DOCTYPE html>
-    // <head><title></title>
-    // </head><body>
-    // <ol>
-    // <li>สร้างโฟลเดอร์ที่จัดเก็บแอป</li>
-    // <li>ใช้คำสั่ง npm init -y</li>
-    // </lo>
-    // </body>
-    // </html>');
+    response.sendFile(path.join(__dirname,'index.html')); // การอ่านไฟล์ indes.html
+    // หากต้องการใช้โมดูล fs ของ Node ทำได้ดังนี้
+
 });
 app.listen(8888);
 console.log('Server started on port:8888');

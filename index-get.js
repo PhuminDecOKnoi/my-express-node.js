a1cb5bc7373d5f5476c7c7b888c33a7b6f0968b8
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-18 23:56:06 +0700

    Update index-get.js

--- index-get.js
+++ index-get.js
@@ -1,19 +1,19 @@
 const app = require("express")(); //loaded module express + load object express.
 const path = require('path'); // load module path.
 
-app.get('/',(request, response)=>{
-    rander(response, 'html/index.html');
-});
-
-app.get('/about',(request, response)=>{
-    rander(response, 'html/about.html');
-});
-
-function rander(response, file){
+function rander(response, file){ // function rander(){}
     response.status(200);
     response.type('text/html');
     response.sendFile(path.join(__dirname, file));
 }
+
+app.get('/',(request, response)=>{ // app.get() - index.html
+    rander(response, 'html/index.html');
+});
+
+app.get('/about',(request, response)=>{ // app.get() - about.html
+    rander(response, 'html/about.html');
+});
 
 app.use((request, response)=>{  // กรณีที่กำหนด Urlไม่ตรงกับ path เลย
     response.status(404);

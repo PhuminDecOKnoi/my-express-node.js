c391a46990843550f86c290808ec780fb2b34bd0
Phumin Decoknoi <phumin.dec@gmail.com>
2023-07-19 22:07:26 +0700

    Update params-get.js

--- params-get.js
+++ params-get.js
@@ -1,14 +1,14 @@
-const { response } = require('express');
+const { response, request } = require('express');
 
 const app = require('express')();
 
 app.get('/', (request,response)=>{
     response.type('text/html');
-    response.send('<a href="/product-detail/24680">Product-Detail</a><hr/></hr.><a href="/search/node.js&express.js/5">Search</a><hr/><a href="/map/10.1122,50.5678/satellite/zoom=16">View Map</a>');
+    response.send('<a href="/product-detail/24680">Product-Detail</a><hr/><a href="/search/node.js&express.js/5">Search</a><hr/><a href="/map/10.1122,50.5678/satellite/zoom=16">View Map</a>');
 });
 
 app.get('/product-detail/:id', (request,response)=>{
-    response.send('product Id' + request.params.id);
+    response.send('product Id=' + request.params.id);
 });
 
 app.get('/search/:q/:page', (request,response)=>{
@@ -19,6 +19,10 @@ app.get('/search/:q/:page', (request,response)=>{
 app.get('/map/:lat,:lon/:type/zoom=:zoom',(request, response)=>{
     response.type('text/html');
     response.send('latitude:'+request.params['lat']+' longtitude: '+request.params['lon'] +' type: '+ request.params.type + ' zoom: '+request.params.zoom);
+});
+
+app.use(request, response =>{
+    response.send('Error 404 : not found');
 });
 
 app.listen(3000,()=>console.log('Server started ...!'));
